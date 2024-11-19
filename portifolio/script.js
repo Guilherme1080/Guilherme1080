d108009af64d24474ccd612f8bc7998953178d3d
Guilherme De Miranda <guilhermemiranda1080@gmail.com>
2024-11-19 15:50:19 -0400

    novo
    novo

--- portifolio/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('dark-mode-toggle').addEventListener('change', function () {
-    if (this.checked) {
-        document.body.style.backgroundColor = '#fff';
-        document.body.style.color = '#000';
-    } else {
-        document.body.style.backgroundColor = '#000';
-        document.body.style.color = '#fff';
-    }
-});
