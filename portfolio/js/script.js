c65ea93f1f53e9e6929f6117fe601ff5d589a041
Guilherme De Miranda <guilhermemiranda1080@gmail.com>
2024-11-25 01:31:34 -0400

    Meu Portifolio
    Meu Portifolio

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,8 +1,9 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('')
-}
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
 
 let sections = document.querySelectorAll('section');
@@ -14,14 +15,39 @@ window.onscroll = () => {
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
+
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
 };
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .projeto-content, .recentes-box, .contato form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .sobre-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .sobre-content', { origin: 'right' });
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Desenvolvedor web','Analista de sistemas','Desenvolvedor back-end', 'Desenvolvedor front-end'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
